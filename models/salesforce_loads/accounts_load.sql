902d11fc2c77f7c33932faa569d7556a0b18a024
James Weakley <james.weakley@omnata.com>
2021-02-05 06:27:23 +0000

    More fixes

--- models/salesforce_loads/accounts_load.sql
+++ models/salesforce_loads/accounts_load.sql
@@ -5,26 +5,13 @@
     materialized='load_task',
     operation='upsert',
     object_name='Account',
-    external_id_field='AccountID__c'
+    external_id_field='AccountNumber__c'
   )
 }}
 
 select OBJECT_CONSTRUCT('Name',NAME,
-        'AccountID__c',ACCOUNT_ID) as RECORD
+        'AccountNumber__c',ACCOUNT_NUMBER) as RECORD
 from {{ ref('accounts') }}
-where 1=1
 
-{% if var('full-refresh-salesforce')==false %}
-
-  -- this filter will only be applied on an incremental run, to prevent re-sync
-  -- of previously successful records
-  and RECORD:"AccountID__c"::varchar not in (
-    select logs.RECORD:"AccountID__c"::varchar 
-    from {{ ref('omnata_push','sfdc_load_task_logs') }} logs
-    where logs.load_task_name= '{{ this.name }}'
-    and logs.RESULT:"success" = true
-  )
-  
-{% endif %}
 
 
--- models/salesforce_loads/contacts_load.sql
+++ models/salesforce_loads/contacts_load.sql
@@ -14,21 +14,5 @@ select OBJECT_CONSTRUCT('FirstName',LAST_NAME,
         'Contact_Number__c',CONTACT_NUMBER,
         'Title',TITLE,
         'Email',EMAIL,
-        'Account',OBJECT_CONSTRUCT('Account_Number__c',ACCOUNT_NUMBER) as RECORD
+        'Account',OBJECT_CONSTRUCT('Account_Number__c',ACCOUNT_NUMBER)) as RECORD
 from {{ ref('contacts') }}
-where 1=1
-
-{% if var('full-refresh-salesforce')==false %}
-
-  -- this filter will only be applied on an incremental run, to prevent re-sync
-  -- of previously successful records
-  and RECORD:"Contact_Number__c"::varchar not in (
-    select logs.RECORD:"Contact_Number__c"::varchar 
-    from {{ ref('omnata_push','sfdc_load_task_logs') }} logs
-    where logs.load_task_name= '{{ this.name }}'
-    and logs.RESULT:"success" = true
-  )
-  
-{% endif %}
-
-
